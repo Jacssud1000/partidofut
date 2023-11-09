7ca51ff6876aed0760b86c875fb7a2b115922d7c
jessierra <jesus.sierra@mercadolivre.com>
2023-11-09 15:38:00 -0300

    test

---
+++ src/main/java/br/com/meli/partidafutbol/PartidaFutbolApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.partidafutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutbolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartidaFutbolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/partidafutbol/PartidaFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.partidafutbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidaFutbolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
